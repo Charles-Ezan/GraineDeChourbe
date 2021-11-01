7e0e3e15a3bbe91e46f87052f9a2ffc478126978
Charles Ezan <charles.ezan1@uqac.ca>
2021-11-01 14:55:17 -0400

    class implementation

---
+++ GraineDeChourbe/Graine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraineDeChourbe
+{
+    class Graine
+    {
+        private int xpos;
+        private int ypos;
+
+        public int get_xpos()
+        {
+            return xpos;
+        }
+
+        public int get_ypos()
+        {
+            return ypos;
+        }
+
+        public (int, int) get_pos()
+        {
+            return (ypos, xpos);
+        }
+    }
+}
